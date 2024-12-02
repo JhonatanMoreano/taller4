fd28234b4c5612217058d583a7ddbf30f7b71310
JhonatanMoreano <u00032246@sise.com.pegift config --listgit config user.name JhonatanMoreanogit config user.email u00032246@gmail.com.pe>
2024-12-01 23:35:03 -0500

    Laboratorio 4 - Ejercicio 1

---
+++ src/main/java/com/semana4/taller4/model/Usuario.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.semana4.taller4.model;
+
+/**
+ *
+ * @author PC
+ */
+public class Usuario {
+    
+    private String nombre;
+    private String correo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
---
+++ src/test/java/com/semana4/taller4/Taller4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semana4.taller4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
